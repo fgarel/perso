0f7cde2ad1bcd65cf980fd61bd87e1f88738729a
Frédéric Garel <frederic.garel@gmail.com>
2014-02-09 20:41:07 +0100

    ajout de rapport de stage

---
+++ source/rapport_stage/mylatex.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+
+#xelatex mylatex.tex
+
+# autre méthode
+# http://vim.wikia.com/wiki/Compiling_LaTeX_from_Vim
+# automatic latex plugin (plugin pour vim)
+# rubber
+# autolatex http://www.arakhne.org/autolatex/index.html
+# latexmk 
+
+# sudo aptitude install autolatex
+#autolatex --xelatex -f mylatex.tex
+autolatex --xelatex -f main.tex
