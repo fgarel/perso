1cf01b8b5ee5e79a3f2aa153a16c2a2db61f7662
Frédéric Garel <fred@tux0>
2015-03-23 21:53:02 +0100

    ajout mkliste.sh

---
+++ dans/bin/mkliste.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+
+DATE='20142015/20150324'
+
+#echo "./mkliste.sh > /media/fred/TOSHIBA\ EXT/MusiqueBretonne/Musique/triParDate/$DATE/liste.txt"
+
+ls -1 /media/fred/TOSHIBA\ EXT/MusiqueBretonne/Musique/triParDate/$DATE/*.mp3 | cut -f10 -d/ | cut -f1,2,3 -d- | cat -n \
+> /media/fred/TOSHIBA\ EXT/MusiqueBretonne/Musique/triParDate/$DATE/liste.txt
+
+
