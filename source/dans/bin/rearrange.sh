49eff5eac323506fe06851ab0f3fff9792067738
Frédéric Garel <frederic.garel@gmail.com>
2015-08-31 21:26:08 +0200

    dans pdf

--- source/dans/bin/rearrange.sh
+++ source/dans/bin/rearrange.sh
@@ -5,3 +5,4 @@ tail -n+2 ../pdf/pdf.txt | sed -e 's/µ/,/g' | csvtool col 3,1,2 - > ../pdf/pdf2
 tail -n+2 ../pdf/video.txt | sed -e 's/µ/,/g' | csvtool col 2,1 - > ../pdf/video2.txt
 
 csvtool join 1 2 ../pdf/pdf2.txt ../pdf/video2.txt 
+csvtool join 1 2 ../pdf/video2.txt ../pdf/pdf2.txt 
