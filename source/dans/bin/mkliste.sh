f74f1e7778ceb2be2e48378eb2912098c12c0e09
Frédéric Garel <frederic.garel@gmail.com>
2015-09-22 07:22:24 +0200

    liste

--- source/dans/bin/mkliste.sh
+++ source/dans/bin/mkliste.sh
@@ -2,7 +2,7 @@
 
 #ls -al $1 | tail -n 10 | cut -d ' ' -f 10
 # =======
-DATE='20152016/20150908'
+DATE='20152016/20150922'
 
 ls -1 /media/fred/TOSHIBA\ EXT/MusiqueBretonne/Musique/triParDate/$DATE/*.mp3 | cut -f10 -d/ | cut -f1,2,3 -d- | cat -n
 
