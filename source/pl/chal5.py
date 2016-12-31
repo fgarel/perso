e9e2f251b522913ea942d58bfa63550c50e3f959
Frédéric Garel <frederic.garel@gmail.com>
2016-12-31 05:56:23 +0100

    modification de chal5

--- source/pl/chal5.py
+++ source/pl/chal5.py
@@ -4,6 +4,7 @@
 #import zipfile,sys,time
 import itertools
 import subprocess
+import sys
 
 def main():
     pass_str =  "abcdefghijklmnopqrstuvwxyz"
@@ -11,15 +12,17 @@ def main():
     pass_str += "0123456789"
     pass_str += "!:$%&/()=?+-*~"
     pass_str += '.;,'
-    #pass_str += '{}[]'
-    #pass_str += '\\'
-    #pass_str += "'"
-    #pass_str += '"'
+    pass_str += '{}[]'
+    pass_str += '\\'
+    pass_str += "'"
+    pass_str += '"'
     #print("pass_str = {}".format(pass_str))
-    print("#!/bin/bash")
+    #print("#!/bin/bash")
 
+    for pass_len in range(1,7):
     #for pass_len in range(2,3):
-    for pass_len in range(6,7):
+    #for pass_len in range(6,7):
+        print("Essai avec les mots de passe de longeur {}".format(pass_len))
         #print("pass_str = {}, pass_len = {}".format(pass_str, pass_len))
         #passwords = itertools.permutations(pass_str,pass_len)
         # ici, on n'utilise pas permutations mais le produit cartesien
@@ -29,17 +32,24 @@ def main():
         for password in passwords:
             password = ''.join(password)
             #print("{}".format(str(password)))
-            chaine = "echo \"{}\" | tee >(./chal5_c8a3333b81d31b1a2053b78623d0c45ada208b05_x86-64.bin >> test.txt) >> test.txt".format(str(password))
-            #print("echo \"{}\" | tee >(./chal5_c8a3333b81d31b1a2053b78623d0c45ada208b05_x86-64.bin >> test.txt) >> test.txt".format(str(password)))
-            print("{}".format(chaine))
+            #chaine = "echo \"{}\" | tee >(./chal5_c8a3333b81d31b1a2053b78623d0c45ada208b05_x86-64.bin >> test.txt) >> test.txt".format(str(password))
+            #print("{}".format(chaine))
+
+            p1 = subprocess.Popen(["echo", password], stdout=subprocess.PIPE)
+            p2 = subprocess.Popen(["tee", "-a", "test.txt"], stdin=p1.stdout, stdout=subprocess.PIPE)
+            p3 = subprocess.Popen(["./chal5_c8a3333b81d31b1a2053b78623d0c45ada208b05_x86-64.bin"], stdin=p2.stdout, stdout=subprocess.PIPE)
+            p4 = subprocess.Popen(["tee", "-a", "test.txt"], stdin=p3.stdout, stdout=subprocess.PIPE)
+            p1.stdout.close()  # Allow p1 to receive a SIGPIPE if p2 exits.
+            retour = p4.communicate()[0]
             #retcode = subprocess.call(chaine, shell=True)
-            #if retcode < 0:
-                #print("Child was terminated by signal", -retcode, file=sys.stderr)
-            #    pass
-            #else:
-                #print("Child returned", retcode, file=sys.stderr)
-                #print("{}".format(password))
-            #    pass
+            if retour == b'Entrez le mot de passe : Mot de passe incorrect.\n':
+                # essaye encore...
+                #print("fail => {}".format(password))
+                pass
+            else:
+                # success !!!!!
+                print("======> {} {}".format(retour, password))
+                break
 
 
 if __name__ == '__main__':
